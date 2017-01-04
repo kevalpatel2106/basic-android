6db0fa311a4e5d962bdd31400c650eb769c7442e
Keval Patel <keval.multidots@gmail.com>
2017-01-04 07:00:00 +0530

    Update RxUtils.java

--- app/src/main/java/com/baseapplication/reactive/RxUtils.java
+++ app/src/main/java/com/baseapplication/reactive/RxUtils.java
@@ -1,6 +1,7 @@
 package com.baseapplication.reactive;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -8,25 +9,25 @@ import rx.schedulers.Schedulers;
 
 /**
  * Created by Keval on 20-Dec-16.
- * All the utils related to Rx.
  *
  * @author {@link 'https://github.com/kevalpatel2106'}
  */
 
 public class RxUtils {
 
-    /**
-     * Create an observable on worker thread and observe it on main thread.
-     *
-     * @param observable observable to run on worker thread.
-     * @param action1    observer to run in UI thread.
-     * @return new {@link Subscription}
-     */
     public static Subscription startObservable(Observable.OnSubscribe observable, Action1 action1) {
         //noinspection unchecked
         return Observable.create(observable)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(action1);
+    }
+
+    public static Subscription startObservable(Observable.OnSubscribe observable, Observer observer) {
+        //noinspection unchecked
+        return Observable.create(observable)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
     }
 }
