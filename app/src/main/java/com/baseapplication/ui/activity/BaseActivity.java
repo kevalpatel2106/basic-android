2536e341981c4b0ca42f6f55456f05f1113a9d10
Keval Patel <kevalpatel2106@gmail.com>
2017-01-29 15:18:54 +0530

    Update BaseActivity.java

--- app/src/main/java/com/baseapplication/ui/activity/BaseActivity.java
+++ app/src/main/java/com/baseapplication/ui/activity/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.baseapplication.ui.activity;
 
 import android.support.annotation.LayoutRes;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.ButterKnife;
 import rx.Subscription;
@@ -19,10 +21,42 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
+
+        //Bind butter knife
         ButterKnife.bind(this);
     }
 
-    protected void addSubscription(Subscription subscription){
+    /**
+     * Set the toolbar of the activity.
+     *
+     * @param toolbarId resource id of the toolbar
+     * @param title     title of the activity
+     */
+    private void setToolbar(int toolbarId, @StringRes int title) {
+        Toolbar toolbar = (Toolbar) findViewById(toolbarId);
+        setSupportActionBar(toolbar);
+
+        setToolbar(title);
+    }
+
+
+    /**
+     * Set the actionbar of the activity.
+     *
+     * @param title title of the activity
+     */
+    protected void setToolbar(@StringRes int title) {
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    /**
+     * Add the subscription to the {@link CompositeSubscription}.
+     *
+     * @param subscription {@link Subscription}
+     */
+    protected void addSubscription(Subscription subscription) {
         mSubscription.add(subscription);
     }
 
